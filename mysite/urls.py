e17f746886abd1437f41ae2c834ef0da312d57ef
Yulia <probachai.yu@gmail.com>
2016-11-28 14:20:07 +0200

    ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from registration.backends.simple.views import RegistrationView
 
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 
     #url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     #url(r'^accounts/register/$', 'registration.views.register', {'form': RegistrationFormUniqueEmail}, name='registration_register'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
    
-    
-]
+
+)
